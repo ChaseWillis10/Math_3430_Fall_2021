29ac07197f6249e7b34f16edb63192e1ff09c969
ChaseWillis10 <90790820+ChaseWillis10@users.noreply.github.com>
2021-11-03 21:35:36 -0500

    Update QR.py

--- QR.py
+++ QR.py
@@ -31,46 +31,81 @@ import LA
 
 #Problem 1
 def unstable_Gram_Schmidt(matrix_A: list[list[complex]]) -> list[list[complex]]:
-    Q = []
-    V = [([0] * (len(matrix_A[0]))) for i in range(len(matrix_A))]
-    R = [[0,0],[0,0]]
-    r: int = 0
-    vector_Hold: list[float] = 0
+    """Finds the Q & R for QR Factorization
 
-    for column in range(len(matrix_A)):
-        V[column] = matrix_A[column]
-        for row in range(0, column):
-            r = LA.inner_product_Result(Q[row], V[column])
-            R[column][row] = r.real
-            vector_Hold = LA.scalar_vector_Multi(Q[row], -R[column][row])
-            V[column] = LA.add_vectors(V[column], vector_Hold)
-        R[column][column] = LA.p_Norm((V[column]))
-        Q.append(LA.scalar_vector_Multi((V[column]), (1 / R[column][column])))
+        Creates four variables, Q which is a blank list, V which is a matrix the size of matrix_A, R which is a square
+        matrix using the number of columns from matrix_A, and lastly vector_Hold which is a blank list. After variables
+        are created it will then run a for loop which will run based off the number of columns in matrix_A. Inside that
+        for loop we overwrite V[column] with matrix_A[column] and then run another for loop. This inner for loop will run
+        from range 0 to what loop the outer for loop is on. The inner for loop will store the inner product of Q and V
+        using our function inner_product_Result from our LA.py file into R. It will then store the scalar vector
+        multiplication of Q and -R using the scalar_vector_Multi function from LA.py into vector_Hold. Then V will be
+        overwritten with the addition of V and vector_Hold using the add_vectors function from LA.py. After that the
+        inner for loop will end and it will then go back to the outer for loop which will then overwrite R with the
+        P Norm of V using the p_Norm function from LA.py. It will then store the scalar vector multiplication of V and
+        1/R into Q. After the outer for loop ends it will then return Q and R.
+
+        Args:
+            matrix_A: A matrix stored as a list of list with complex and\or normal numbers.
+
+        Returns:
+            The Q & R for QR factorization
+
+    """
+
+    Q: list[float] = []
+    V: list[list[float]] = [([0] * (len(matrix_A[0]))) for i in range(len(matrix_A))]
+    R: list[list[float]] = [([0] * (len(matrix_A))) for i in range(len(matrix_A))]
+    vector_Hold: list[float] = []
+
+    for Outer_For in range(len(matrix_A)):
+        V[Outer_For] = matrix_A[Outer_For]
+        for Inner_For in range(0, Outer_For):
+            R[Outer_For][Inner_For] = LA.inner_product_Result(Q[Inner_For], V[Outer_For])
+            vector_Hold = LA.scalar_vector_Multi(Q[Inner_For], -R[Outer_For][Inner_For])
+            V[Outer_For] = LA.add_vectors(V[Outer_For], vector_Hold)
+        R[Outer_For][Outer_For] = LA.p_Norm((V[Outer_For]))
+        Q.append(LA.scalar_vector_Multi((V[Outer_For]), (1 / R[Outer_For][Outer_For])))
     return [Q,R]
-print("Problem 1")
-print(unstable_Gram_Schmidt([[1,0,1],[2,1,0]]))
 
 #Problem 2
 
-def stable_Gram_Schmidt(matrix_A):
-    Q = []
-    V = []
-    R = [[0, 0], [0, 0]]
-    r = 0
-    scal = 0
+def stable_Gram_Schmidt(matrix_A: list[list[complex]]) -> list[list[complex]]:
+    """Finds the Q & R for QR Factorization
+
+            Creates four variables, Q which is a blank list, V which is a blank list, R which is a square
+            matrix using the number of columns from matrix_A, and lastly vector_Hold which is a blank list. After
+            variables are created we will run a for loop which will put all the element in matrix_A into our V variable.
+            After that it will then run a for loop which will run based off the number of columns in matrix_A. Inside
+            that for loop it will overwrite R with the P Norm of V using the p_Norm function from LA.py. It will then
+            store the scalar vector multiplication of V and 1/R into Q. After this another for loop will then run. This
+            inner for loop will run from range of what loop the outer for loop is on plus one to the length of matrix_A.
+            The inner for loop will store the inner product of Q and V using our function inner_product_Result from our
+            LA.py file into R. It will then store the scalar vector multiplication of Q and -R using the
+            scalar_vector_Multi function from LA.py into vector_Hold. Then V will be overwritten with the addition of V
+            and vector_Hold using the add_vectors function from LA.py. After the outer for loop ends it will then return
+            Q and R.
+
+            Args:
+                matrix_A: A matrix stored as a list of list with complex and\or normal numbers.
+
+            Returns:
+                The Q & R for QR factorization
+
+        """
+
+    Q: list[complex] = []
+    V: list[complex] = []
+    R: list[list[float]] = [([0] * (len(matrix_A))) for i in range(len(matrix_A))]
+    vector_Hold: list[float] = []
+
     for element in matrix_A:
         V.append(element)
-    for k in range(len(matrix_A)):
-        R[k][k] = LA.p_Norm((V[k]))
-        Q.append(LA.scalar_vector_Multi((V[k]), (1 / R[k][k])))
-        for j in range(k+1, len(matrix_A)):
-            print(j)
-            print(k)
-            r = LA.inner_product_Result(Q[k], V[j])
-            R[j][k] = r.real
-            scal = LA.scalar_vector_Multi(Q[k], -R[j][k])
-            V[j] = LA.add_vectors(V[j], scal)
+    for Outer_For in range(len(matrix_A)):
+        R[Outer_For][Outer_For] = LA.p_Norm((V[Outer_For]))
+        Q.append(LA.scalar_vector_Multi((V[Outer_For]), (1 / R[Outer_For][Outer_For])))
+        for Inner_For in range(Outer_For+1, len(matrix_A)):
+            R[Inner_For][Outer_For] = LA.inner_product_Result(Q[Outer_For], V[Inner_For])
+            vector_Hold = LA.scalar_vector_Multi(Q[Outer_For], -R[Inner_For][Outer_For])
+            V[Inner_For] = LA.add_vectors(V[Inner_For], vector_Hold)
     return [Q,R]
-
-print("Problem 2")
-print(stable_Gram_Schmidt([[1,0,1],[2,1,0]]))
