f5f06e838c1fc6ae2ed9efa58eb3a1ec8f0f0806
ChaseWillis10 <90790820+ChaseWillis10@users.noreply.github.com>
2021-11-28 20:02:42 -0600

    Update test_LS.py

--- test_LS.py
+++ test_LS.py
@@ -1,1 +1,10 @@
+import LS
+import pytest
 
+def test_least_squares():
+    matrix_A = [[3,4,0],[-6,-8,1]]
+    vector_A = [-1,7,2]
+    matrix_B = [[1,2,3],[4,5,6],[7,8,9]]
+    vector_B = [1,2,3]
+    assert LS.least_Squares(matrix_A, vector_A) == [(5+0j), (1.9999999999999991+0j)] #pytest.approx([(5+0j),(2+0j)], .001)
+    assert LS.least_Squares(matrix_B, vector_B) == []
